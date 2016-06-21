30c9da41ff171876f8db272810cfb6dab3ca2ad1
lukejacksonn <lukejacksonn@gmail.com>
2016-06-21 11:25:54 -0400

    Swapped out count for random int and used ES6 string interpolation to format url

--- popup.js
+++ popup.js
@@ -1,7 +1,5 @@
-var a = 0;
-function count() {
-	console.log("help!");
-	a++;
-	document.getElementById("catpictures").innerHTML='<a href="http://thecatapi.com"><img src="http://thecatapi.com/api/images/get?format=src&size=small&type=gif&dummy='+a+'"></a>';
+function randomCat() {
+	document.getElementById("catpictures").innerHTML =
+		`<img src="http://thecatapi.com/api/images/get?format=src&size=small&type=gif&dummy=${Math.random()*10}">`;
 }
-document.getElementById('gobutton').onclick = count;
+document.getElementById('gobutton').onclick = randomCat;
